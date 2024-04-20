983acb35790235dd766ae2835b018128166b4206
dhulipall <Dhulipallaraviteja9640@gmail.com>
2024-04-20 13:44:08 +0530

    fix url error

--- college_management_system/college_management_system/urls.py
+++ college_management_system/college_management_system/urls.py
@@ -27,3 +27,5 @@ urlpatterns = [
     path("get_attendance", views.get_attendance, name='get_attendance'),
     path("firebase-messaging-sw.js", views.showFirebaseJS, name='showFirebaseJS'),
     path("doLogin/", views.doLogin, name='user_login')
+]
+
